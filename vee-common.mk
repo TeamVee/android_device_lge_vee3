4d0827f19f00253fa3c0f757ac372cf6d53b8527
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-01-10 13:10:35 -0200

    Ops sorry

--- vee-common.mk
+++ vee-common.mk
@@ -32,7 +32,7 @@ DEVICE_PACKAGE_OVERLAYS += device/lge/vee-common/overlay
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 
 # Fix Audio Compilation
-PRODUCT_COPY_FILES += vendor/lge/vee3/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so
+PRODUCT_COPY_FILES += vendor/lge/vee-common/proprietary/lib/libaudioalsa.so:obj/lib/libaudioalsa.so
 PRODUCT_COPY_FILES += vendor/lge/vee-common/proprietary/lib/libaudcal.so:obj/lib/libaudcal.so
 
 # Rootdir files
