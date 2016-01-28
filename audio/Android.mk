949704300095974f374bad89f59a8b8c8100eda1
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-01-28 15:43:38 -0200

    Revert "Revert "audio/msm7x27a: Remove deprecated/unsupported ANC headset code""
    This reverts commit 2d12a7d3690c62ac763dcfb2b390dbfb05cf67ce.

--- audio/Android.mk
+++ audio/Android.mk
@@ -6,10 +6,6 @@
 LOCAL_PATH := $(call my-dir)
 
 common_cflags := -D_POSIX_SOURCE
-ifneq ($(strip $(QCOM_ANC_HEADSET_ENABLED)),false)
-    common_cflags += -DQCOM_ANC_HEADSET_ENABLED
-endif
-
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_FM)),true)
     common_cflags += -DQCOM_FM_ENABLED
 endif
