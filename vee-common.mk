37a49f627d94b872164e317949b40245b6903257
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-02-01 16:28:51 -0200

    vee-common: We still need you

--- vee-common.mk
+++ vee-common.mk
@@ -18,6 +18,10 @@
 #
 # Everything in this directory will become public
 
+# Density
+ PRODUCT_AAPT_CONFIG := normal
+ PRODUCT_AAPT_PREF_CONFIG := ldpi
+ 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
