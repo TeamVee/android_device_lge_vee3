1ba7ad913fae002cb25fbd48a506903d9858c71d
Caio Oliveira <caiooliveirafarias0@gmail.com>
2016-04-30 11:49:49 -0300

    vee3: We have problems with that line, lets try with this
    Signed-off-by: Caio Oliveira <caiooliveirafarias0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,7 +132,7 @@ MALLOC_IMPL := dlmalloc
 
 # Enable dex-preoptimization to speed up first boot sequence
 WITH_DEXPREOPT := true
-WITH_DEXPREOPT_BOOT_IMG_ONLY := true
+DONT_DEXPREOPT_PREBUILTS := true
 
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
